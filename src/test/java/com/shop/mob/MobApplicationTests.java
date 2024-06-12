e9d0c085b4c348655592f1f1cce6cc931277cd02
Saifudheen <Saifudheen@sellowpay.com>
2024-06-12 11:43:38 +0530

    feat: Initial project setup with application properties, database connection

---
+++ src/test/java/com/shop/mob/MobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.mob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
